70e693fab87103ac33bbd59253730356525bbb2c
xanh <nvxanh75@gmail.com>
2025-04-16 13:00:45 +0700

    first commit

---
+++ src/main/java/com/example/G_Scores/GScoresApplication.java
@@ -0,0 +1,13 @@
+package com.example.G_Scores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GScoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GScoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/G_Scores/GScoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.G_Scores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GScoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
